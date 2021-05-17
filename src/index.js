284f990c23c1719b2b5424ccb1dc5432082c4503
lavish-jain <lavishrjain1997@gmail.com>
2021-05-17 19:37:19 +0530

    added css, css-loader and style-loader modules for webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { run } from "./app/app";
 import { Alert } from "./app/services/alert";
 import { Component } from "./app/services/component";
+import "./main.css";
 
 const alertService = new Alert();
 const componentService = new Component();
