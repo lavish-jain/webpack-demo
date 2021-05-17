1ddad149064064924ec103ad5f0e3dd7a3e0c6d0
lavish-jain <lavishrjain1997@gmail.com>
2021-05-17 20:03:47 +0530

    Added contenthash, html template, htmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "main.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
@@ -14,4 +15,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };
